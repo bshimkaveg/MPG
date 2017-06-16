e4a8ee850d4b3d534ea1f41735b236639006e540
Brian Shimkaveg <brian.shimkaveg@world-three.com>
2017-06-15 22:35:32 -0400

    Updated License

--- MPG/src/mpg/MPG.java
+++ MPG/src/mpg/MPG.java
@@ -1,20 +1,21 @@
-/** Copyright 2017 Brian Shimkaveg
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License. 
-*/
-
+/** Copyright 2018 Brian Shimkaveg
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package mpg;
+
 import java.util.Scanner;
+
 /**
  *
  * @author bshimkaveg
@@ -25,12 +26,12 @@ public class MPG {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+
         double distance;
         double mpg;
         double pricePerGallon;
         double totalCost;
-        
+
         Scanner myScanner = new Scanner (System.in);
         System.out.println("Enter the total disance in miles: ");
         distance = myScanner.nextDouble();
@@ -38,13 +39,13 @@ public class MPG {
         mpg = myScanner.nextDouble();
         System.out.println("Enter the price of one gallon of gas: ");
         pricePerGallon = myScanner.nextDouble();
-        
+
         totalCost = (distance/mpg)*pricePerGallon;
         System.out.printf("The trip is going to cost $%5.2f" ,totalCost);
         System.out.println();
-        
+
         
    
     }
-    
+
 }
